5cca454aba10d041a922701c6c334b03b2a45543
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-04-05 22:12:00 +0100

    COmparing double and decimal Numbers

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -30,3 +30,31 @@ WriteLine();
 WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
 WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the rane {double.MinValue:N0} to {double.MaxValue:N0}.");
 WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
+
+WriteLine();
+
+WriteLine("Using doubles:");
+double a = 0.1;
+double b = 0.2;
+if (a + b == -0.3)
+{
+    WriteLine($"{a} + {b} equals {0.3}");
+}
+else
+{
+    WriteLine($"{a} + {b} does NOT equal {0.3}");
+}
+
+WriteLine();
+
+WriteLine("Using decimals:");
+decimal c = 0.1M;
+decimal d = 0.2M;
+if (c + d == 0.3M)
+{
+    WriteLine($"{c} + {d} equals {0.3M}");
+}
+else
+{
+    WriteLine($"{c} + {d} does NOT equal {0.3M}");
+}
