6d6e2355a582fe10aae6b8798b4aa9f32e5a4a11
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-04-26 22:34:01 +0100

    Understanding format strings

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -30,3 +30,17 @@ WriteLine();
 // expression but not in the string text.
 WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
 
+WriteLine();
+
+string applesText = "Apples";
+int applesCount = 1234;
+string bananasText = "Bananas";
+int bananasCount = 56789;
+WriteLine(format: "{0,-10} {1,6}",
+arg0: "Name", arg1: "Count");
+WriteLine(format: "{0,-10} {1,6:N0}",
+arg0: applesText, arg1: applesCount);
+WriteLine(format: "{0,-10} {1,6:N0}",
+arg0: bananasText, arg1: bananasCount);
+
+
