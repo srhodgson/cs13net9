2f2c32ca47d94e2171d5bc7664e38c98ba3ac4e5
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-05-07 22:27:28 +0100

    Number formatting & getting user input

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -43,4 +43,24 @@ arg0: applesText, arg1: applesCount);
 WriteLine(format: "{0,-10} {1,6:N0}",
 arg0: bananasText, arg1: bananasCount);
 
+WriteLine();
+
+decimal value = 0.325M;
+WriteLine("Currency: {0:C}, Percentage: {0:0.0%}", value);
+
+WriteLine();
+
+/*WriteLine("Type your first name and press ENTER: ");
+string? firstName = ReadLine();
+WriteLine("Type you age and press ENTER: ");
+string age = ReadLine()!;
+WriteLine($"Hello {firstName}, you look good for {age}.");*/
+
+WriteLine();
+
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+    arg0: key.Key, arg1: key.KeyChar, arg2: key.Modifiers);
 
