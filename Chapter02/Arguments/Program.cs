a1b058d39e5c65d2b7c830a3ee37efe6253f4180
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-05-09 22:39:51 +0100

    Add exercises

--- Chapter02/Arguments/Program.cs
+++ Chapter02/Arguments/Program.cs
@@ -1,6 +1,28 @@
-﻿WriteLine($"There are {args.Length} arguments.");
+﻿//WriteLine($"There are {args.Length} arguments.");
 
-foreach (string arg in args)
+//foreach (string arg in args)
+//{
+//    WriteLine(arg);
+//}
+
+if (args.Length < 3)
 {
-    WriteLine(arg);
+    WriteLine("You must specify two colours and cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return; // stop running
+}
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0], ignoreCase: true);
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1], ignoreCase: true);
+
+try
+{
+    CursorSize = int.Parse(args[2]);
+}
+catch (PlatformNotSupportedException)
+{
+    WriteLine("The current platform does not support changing the size of the cursor");
 }
