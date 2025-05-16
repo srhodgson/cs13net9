363755e52d647895a4f7a87862a68bae29127d70
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-05-16 22:20:13 +0100

    Branching with the if statement

---
+++ Chapter03/SelectionStatements/Program.cs
@@ -0,0 +1,12 @@
+﻿#region Branching with the if statement 
+string password = "ninja";
+if (password.Length < 8)
+{
+    WriteLine("Your password is too short. Use at least 8 chars.");
+}
+else
+{
+    WriteLine("Your password is strong.");
+}
+#endregion 
+
