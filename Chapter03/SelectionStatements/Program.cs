e1fb4ab1b71cf3f8cc24e4f08ad9b39331393f2f
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-05-16 22:25:52 +0100

    Pattern matching with the if statement

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -10,3 +10,16 @@ else
 }
 #endregion 
 
+#region Pattern matching with the if statement
+// Add and remove the " " to change between string and int 
+object o = 3;
+int j = 4;
+if (o is int i)
+{
+    WriteLine($"{i} x {j} = {i * j}");
+}
+else
+{
+    WriteLine("o is not an int so it cannot multiply!");
+}
+#endregion 
