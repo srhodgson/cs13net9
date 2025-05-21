350d80bfd4003282fbb85161c4961ed562e8ec38
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-05-21 22:25:29 +0100

    Looping with the do statement

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -1,6 +1,21 @@
-﻿int x = 0;
+﻿#region Looping with the while statement
+int x = 0;
 while (x < 10)
 {
     WriteLine(x);
     x++;
 }
+#endregion
+
+WriteLine();
+
+#region Looping with the do statement
+string? actualPassword = "Pa$$w0rd";
+string? password;
+do
+{
+    Write("Enter your password: ");
+    password = ReadLine();
+} while (password != actualPassword);
+WriteLine("Correct!");
+#endregion
