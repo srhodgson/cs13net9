d22d911ae4ad2cb70f9a01fc18127570d329d830
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-05-21 22:36:25 +0100

    Looping with the for statement

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -10,12 +10,29 @@ while (x < 10)
 WriteLine();
 
 #region Looping with the do statement
-string? actualPassword = "Pa$$w0rd";
+/*string? actualPassword = "Pa$$w0rd";
 string? password;
 do
 {
     Write("Enter your password: ");
     password = ReadLine();
 } while (password != actualPassword);
-WriteLine("Correct!");
+WriteLine("Correct!");*/
+#endregion
+
+WriteLine();
+
+#region Looping with the for statement
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
+
+WriteLine();
+
+for (int y = 0; y <= 10; y += 3)
+{
+    WriteLine(y);
+}
 #endregion
