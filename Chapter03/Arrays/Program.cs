6ea5fa90aff59d54a7db44820c035a8b2a797361
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-06-06 22:28:31 +0100

    Jagged arrays

--- Chapter03/Arrays/Program.cs
+++ Chapter03/Arrays/Program.cs
@@ -36,3 +36,32 @@ for (int row = 0; row <= grid1.GetUpperBound(0); row++)
         WriteLine($"Row {row}, Column {col}: {grid1[row, col]}");
     }
 }
+
+WriteLine();
+
+string[][] jagged =
+{
+    new[] { "Alpha", "Beta", "Gamma" },
+    new[] { "Anne", "Ben", "Charlie", "Doug" },
+    new[] { "Aardvark", "Bear" }
+};
+
+WriteLine("Upper bound of the array of arrays is: {0}",
+    jagged.GetUpperBound(0));
+
+for (int array = 0; array <= jagged.GetUpperBound(0); array++)
+{
+    WriteLine("Upper bound of array {0} is: {0}",
+        arg0: array,
+        arg1: jagged[array].GetUpperBound(0));
+}    
+
+WriteLine();
+
+for (int row = 0; row <= jagged.GetUpperBound(0); row++)
+{
+    for (int col = 0; col <= jagged[row].GetUpperBound(0); col++)
+    {
+        WriteLine($"Row {row}, Column {col}: {jagged[row][col]}");
+    }
+}
