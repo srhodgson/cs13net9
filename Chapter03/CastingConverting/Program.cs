668ecb2b249558c436c2206bdfc12b0a792d7e0b
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-06-20 21:50:00 +0100

    How negative numbers are represented in binary

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -6,9 +6,26 @@ double c = 9.8;
 int d = (int)c; // Compiler gives an error if you do not explicitly cast.
 WriteLine($"c is {c}, d is {d}");
 
-long e = 10;
+long e = 5_000_000_000;
 int f = (int)e;
 WriteLine($"e is {e:N0}, f si {f:N0}");
 e = long.MaxValue;
 f = (int)e;
 WriteLine($"e is {e:N0}, f is {f:N0}");
+
+WriteLine();
+
+WriteLine($"{0,12} {1,34}", "Decimal", "Binary");
+WriteLine($"{0,12} {0,34:B32}", int.MaxValue);
+for (int i = 8; i >= -8; i--)
+{
+    WriteLine("{0,12} {0,34:B32}", i);
+}
+WriteLine("{0,12} {0,34:B32}", int.MinValue);
+
+WriteLine();
+
+long r = 0b_101000101010001100100111010100101010;
+int s = (int)r;
+WriteLine($"{r,38:B38} = {r}");
+WriteLine($"{s,38:B32} = {s}");
