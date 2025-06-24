252315453a07ed29dd5a8697b4ae61878bf67ed5
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-06-24 22:46:38 +0100

    Catching specific exceptions

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -6,6 +6,14 @@ try
     int age = int.Parse(input!);
     WriteLine($"You are {age} years old.");
 }
+catch (OverflowException)
+{
+    WriteLine("Your age is a valid format but it is either too big or small");
+}
+catch (FormatException)
+{
+    WriteLine("The age you entered is not a valid number format.");
+}
 catch (Exception ex)
 {
     WriteLine($"{ex.GetType()} says {ex.Message}");
