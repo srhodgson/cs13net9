2064ab3b141628532498755d989863765d5e0b17
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-06-25 22:51:35 +0100

    Start chapter 04

---
+++ Chapter04/TopLevelFunctions/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+WriteLine("* Top-level functions example");
+WhatsMyNamespace(); // Call the function
+
+void WhatsMyNamespace() // Define a local function
+{
+    WriteLine("Namespace of Program class: {0}",
+        arg0: typeof(Program).Namespace ?? "null");
+}
