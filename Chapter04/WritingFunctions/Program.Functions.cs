15827b7eb86548004593917f19c6d38c4595ca6b
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-06-28 22:45:28 +0100

    Cardinal to ordinal

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -39,4 +39,26 @@ partial class Program
         }
         WriteLine($"CurrentCulture: {CultureInfo.CurrentCulture.DisplayName}");
     }
+
+    static string CardinalToOrdinal(uint number)
+    {
+        uint lastTwoDigits = number % 100;
+        switch (lastTwoDigits)
+        {
+            case 11:  // Special cases for 11th to 13th 
+            case 12:
+            case 13:
+                return $"{number:N0}th";
+            default:
+                uint lastDigit = number % 10;
+                string suffix = lastDigit switch
+                {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th"
+                };
+                return $"{number:N0}{suffix}";
+        }
+    }
 }
