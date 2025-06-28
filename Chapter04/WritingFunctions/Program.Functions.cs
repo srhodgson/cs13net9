999c3dee93e2ef6f686b0fd72362477786981295
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-06-28 22:34:55 +0100

    update to french culture

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -1,3 +1,5 @@
+using System.Globalization; // To use CultureInfo
+
 partial class Program
 {
     static void TimesTable(byte number, byte size = 12)
@@ -9,5 +11,32 @@ partial class Program
             WriteLine($"{row} x {number} = {row * number}");
         }
         WriteLine();
+    }
+
+    static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
+    {
+        decimal rate = twoLetterRegionCode switch
+        {
+            "CH" => 0.08M, // Switzerland 
+            "DK" or "NO" => 0.25M, // Denmark, Norway
+            "GB" or "FR" => 0.2M, // UK, France
+            "HU" => 0.27M, // Hungary
+            "OR" or "AK" or "MT" => 0.0M, // Oregon, Alaska, Montanta
+            "ND" or "WI" or "ME" or "VA" => 0.05M, // North Dakota, Wisconsin, Maine, Virginia
+            "CA" => 0.0825M, // California 
+            _ => 0.06M // Most other states
+        };
+        return amount * rate;
+    }
+
+    static void ConfigureConsole(string culture = "fr-FR", bool useComputerCulture = true)
+    {
+        // To enable Unicode characters like Euro symbol in the console 
+        OutputEncoding = System.Text.Encoding.UTF8;
+        if (!useComputerCulture)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+        }
+        WriteLine($"CurrentCulture: {CultureInfo.CurrentCulture.DisplayName}");
     }
 }
--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,1 +1,12 @@
-﻿TimesTable(7,20);
+﻿//TimesTable(7,20);
+
+WriteLine();
+
+ConfigureConsole();
+
+WriteLine();
+
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay:C} in tax.");
+// Alternatively, call the function in the interpolated string.
+// Writeline($"You must pay {CalculateTax{amount: 149, twoLetterRegionCode: "FR"):C} in tax.");
