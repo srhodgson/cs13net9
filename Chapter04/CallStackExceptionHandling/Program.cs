fcc5903a338c2f3564ce58d9ae8d9653378cec80
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-07-04 22:50:53 +0100

    Understanding the call stack

---
+++ Chapter04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Configuration.Assemblies;
+using CallStackExceptionHandlingLib;
+
+WriteLine("In Main");
+Alpha();
+
+void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+
+void Beta()
+{
+    WriteLine("In Beta");
+    Processor.Gamma();
+}
---
+++ Chapter04/CallStackExceptionHandlingLib/Processor.cs
@@ -0,0 +1,16 @@
+﻿namespace CallStackExceptionHandlingLib;
+
+public class Processor
+{
+    public static void Gamma() // Public so it can be called from outside.
+    {
+        WriteLine("In Gamma");
+        Delta();
+    }
+
+    private static void Delta() // Private so it can only be called internally
+    {
+        WriteLine("In Delta");
+        File.OpenText("bad file path");
+    }
+}
