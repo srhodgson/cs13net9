43844336cce6c975a8ec09abd6f3a83cc44f7529
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-07-08 22:06:02 +0100

    instantiate some people

--- Chapter05/PacktLibraryNet2/Person.cs
+++ Chapter05/PacktLibraryNet2/Person.cs
@@ -1,7 +1,10 @@
 // All types in this file will be defined in this file-scoped name-space 
 namespace Packt.Shared;
 
-public class Person
+public class Person : Object
 {
-    
+    #region Fields: Data or state for this person.
+    public string? Name; // ? means it can be null.
+    public DateTimeOffset Born;
+    #endregion
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -11,3 +11,21 @@ ConfigureConsole(); // Sets current culture to US English
 Person bob = new(); // C# 9 or later 
 WriteLine(bob); // Implicit call ToString().
 // WriteLine(bob.ToString()); // Does the same thing 
+
+bob.Name = "Bob Smith";
+bob.Born = new DateTimeOffset(
+    year: 1965, month: 12, day: 22,
+    hour: 16, minute: 28, second: 0,
+    offset: TimeSpan.FromHours(-5)); // US Eastern Standard Time
+WriteLine(format: "{0} was born on {1:D}.",
+    arg0: bob.Name, arg1: bob.Born);
+
+WriteLine();
+
+Person aliuce = new()
+{
+    Name = "Alice Jones",
+    Born = new(1998, 3, 7, 16, 28, 0, TimeSpan.Zero)
+};
+WriteLine(format: "{0} was born on {1:d}.", // short date 
+    arg0: aliuce.Name, arg1: aliuce.Born);
