475199206568f34bfc8edaf8035b9bbba4ce538d
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-07-08 22:17:11 +0100

    enum

--- Chapter05/PacktLibraryNet2/Person.cs
+++ Chapter05/PacktLibraryNet2/Person.cs
@@ -6,5 +6,6 @@ public class Person : Object
     #region Fields: Data or state for this person.
     public string? Name; // ? means it can be null.
     public DateTimeOffset Born;
+    public WondersOfTheAncientWorld FavouriteAncientWonder;
     #endregion
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -17,15 +17,20 @@ bob.Born = new DateTimeOffset(
     year: 1965, month: 12, day: 22,
     hour: 16, minute: 28, second: 0,
     offset: TimeSpan.FromHours(-5)); // US Eastern Standard Time
+bob.FavouriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
 WriteLine(format: "{0} was born on {1:D}.",
     arg0: bob.Name, arg1: bob.Born);
+WriteLine(format: "{0}'s favorite wonder is {1}. It's integer is {2}.",
+    arg0: bob.Name,
+    arg1: bob.FavouriteAncientWonder,
+    arg2: (int)bob.FavouriteAncientWonder);
 
 WriteLine();
 
-Person aliuce = new()
+Person alice = new()
 {
     Name = "Alice Jones",
     Born = new(1998, 3, 7, 16, 28, 0, TimeSpan.Zero)
 };
 WriteLine(format: "{0} was born on {1:d}.", // short date 
-    arg0: aliuce.Name, arg1: aliuce.Born);
+    arg0: alice.Name, arg1: alice.Born);
