83aa4c0e2257d0f31dac4720fcfd7f4dda44229e
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-08-06 22:11:10 +0100

    new chapter & storing any object

---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,11 @@
+﻿#region Storing any type of object
+object height = 1.88; // Storing a double in an object 
+object name = "Amir"; // Storing a string in an object 
+WriteLine($"{name} is {height} metres tall.");
+//int length1 = name.Length; // This gives a compile error 
+int length2 = ((string)name).Length; // Cast name to a string 
+WriteLine($"{name} has {length2} characters.");
+#endregion 
+
+WriteLine();
+
