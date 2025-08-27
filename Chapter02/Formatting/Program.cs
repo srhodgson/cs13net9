9c09e3ebf3428fb92681cdd16d3d2b6f4f90c144
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-08-27 22:06:56 +0100

    Understanding format strings
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -5,7 +5,7 @@ CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
 int numberOfApples = 12;
 decimal pricePerApple = 0.35M;
 
-Console.WriteLine(
+WriteLine(
     format: "{0} apples cost {1:C}.",
     arg0: numberOfApples,
     arg1: pricePerApple * numberOfApples
@@ -20,9 +20,33 @@ string formatted = string.Format(
 // WriteToFIle(formatted); // Write the string into a file
 
 // Three parameter values can use named arguments s
-Console.WriteLine("{0} {1} lived in {2}.",
+WriteLine("{0} {1} lived in {2}.",
 arg0: "Roger", arg1: "Cevung", arg2: "Stockholm");
 
 // Four or more parameter values cannot use named arguments 
-Console.WriteLine("{0} {1} lived in {2} and worked in the {3} team at {4}.",
+WriteLine("{0} {1} lived in {2} and worked in the {3} team at {4}.",
 "Roger", "Cevung", "Stockholm", "Education", "Optimizely");
+
+WriteLine();
+
+// The following statement must be all on one line when using C# 10 or earlier.
+// If using C# 11 or later, we can include a line break in the middle of an 
+// expression but not in the string text
+WriteLine($"{numberOfApples} apples cost {pricePerApple
+* numberOfApples:C}");
+
+WriteLine();
+
+#region Understanding format strings
+string applesText = "Apples";
+int applesCount = 1234;
+string bananasText = "Bananas";
+int bananasCount = 56789;
+WriteLine();
+WriteLine(format: "{0,-10} {1,6}",
+arg0: "Name", arg1: "Count");
+WriteLine(format: "{0,-10} {1,6:N0}",
+arg0: applesText, arg1: applesCount);
+WriteLine(format: "{0,-10} {1,6:N0}",
+arg0: bananasText, arg1: bananasCount);
+#endregion
