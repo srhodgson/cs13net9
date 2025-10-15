881497ab5bb7270a391817a86d36011b5b59aa52
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-10-15 23:27:47 +0100

    Various types of operators
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,48 @@
+﻿#region Exploring unary operators 
+int a = 3;
+int b = a++;
+WriteLine($"a is {a}, b is {b}");
+
+int c = 3;
+int d = ++c; // Prefix means increment c before assigning it.
+WriteLine($"c is {c}, d is {d}");
+#endregion
+
+#region Exploring binary arithmetic operators 
+WriteLine();
+
+int e = 11;
+int f = 3;
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e - f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+
+WriteLine();
+
+double g = 11.0;
+WriteLine($"g is {g:N1}, f is {f}");
+WriteLine($"g / f = {g / f}");
+
+WriteLine();
+#endregion
+
+
+#region Exploring logical operators 
+bool p = true;
+bool q = false;
+
+WriteLine($"AND    | P     | q    ");
+WriteLine($"p      | {p & p,-5} | {p & q,-5}  ");
+WriteLine($"q      | {q & p,-5} | {q & q,-5}  ");
+WriteLine();
+WriteLine($"OR     | p     | q    ");
+WriteLine($"p      | {p | q,-5} | {p | q,-5}  ");
+WriteLine($"q      | {q | p,-5} | {q | q,-5}  ");
+WriteLine();
+WriteLine($"XOR    | p     | q    ");
+WriteLine($"p      | {p ^ p,-5} | {p ^ q,-5}  ");
+WriteLine($"q      | {q ^ p,-5} | {q ^ q,-5}  ");
+#endregion
