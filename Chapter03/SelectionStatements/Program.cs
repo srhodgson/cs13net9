51dffdeab6575c8d5b0ebbc262597323e7b8045c
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-10-20 22:37:33 +0100

    switch statements

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -26,4 +26,74 @@ else
 {
     WriteLine("o is not an int so it cannot multiply!");
 }
+
+WriteLine();
+#endregion
+
+#region Branching with the switch statement
+// Inclusive lower bound but exclusive upper bound.
+int number = Random.Shared.Next(minValue: 1, maxValue: 7);
+WriteLine($"My random number is {number}");
+switch (number)
+{
+    case 1:
+        WriteLine("One");
+        break; // Jumps to end of switch statement 
+    case 2: 
+        WriteLine("Two");
+        goto case 1;
+    case 3: // Multiple case section
+    case 4:
+        WriteLine("Three or Four");
+        goto case 1;
+    case 5:
+        goto A_label;
+    default:
+        WriteLine("Default");
+        break;
+} // end of switch statement 
+
+WriteLine("After end of switch");
+A_label:
+WriteLine($"After A_label");
+WriteLine();
+#endregion 
+
+#region pattern matching with the switch statement
+
+var animals = new Animal?[]
+{
+    new Cat { Name = "Karen", Born = new(year: 2022, month: 8, day: 23), Legs = 4, IsDomestic = true },
+    null,
+    new Cat { Name = "Mufasa", Born = new(year: 1994, month: 6, day: 12) },
+    new Spider { Name = "Sid Vicious", Born = DateTime.Today, IsVenomous = true },
+    new Spider { Name = "Captain Furry", Born = DateTime.Today }
+};
+
+foreach (Animal? animal in animals)
+{
+    string message;
+    switch (animal)
+    {
+        case Cat fourLeggedCat when fourLeggedCat.Legs == 4:
+            message = $"The cat named {fourLeggedCat.Name} has four legs.";
+            break;
+        case Cat wildCat when wildCat.IsDomestic == false:
+            message = $"The non-domestic cat is named {wildCat.Name}.";
+            break;
+        case Cat cat:
+            message = $"The cat is name {cat.Name}.";
+            break;
+        default: // default is always evaluated last.
+            message = $"{animal.Name} is a {animal.GetType().Name}.";
+            break;
+        case Spider spider when spider.IsVenomous:
+            message = $"The {spider.Name} spider is venomous. Run!";
+            break;
+        case null:
+            message = "The animal is null.";
+            break;
+    }
+    WriteLine($"switch statement: {message}");
+}
 #endregion
