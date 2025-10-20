5ba0deba8ec6099022aa3d767d20f347694b68b7
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-10-20 21:45:33 +0100

    if statements

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#region branching with the if statement
+string password = "ninja";
+if (password.Length < 8)
+{
+    WriteLine("Your password is too short. Use at least 8 chars.");
+}
+else
+{
+    WriteLine("your password is strong.");
+}
 
-Console.WriteLine("Hello, World!");
+WriteLine();
+#endregion 
+
+#region pattern matching with the is statement
+// Add and remove the "" to change between string and int.
+
+object o = 3;
+int j = 4;
+
+if (o is int i)
+{
+    WriteLine($"{i} x {j} = {i * j}");
+}
+else
+{
+    WriteLine("o is not an int so it cannot multiply!");
+}
+#endregion
