c703b4be5b3d0efec59b77be0cbc7d27c3d34039
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-10-28 23:07:44 +0000

    Arrays and stuff
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ Chapter03/Arrays/Program.cs
@@ -0,0 +1,55 @@
+﻿#region Working with single-dimensional arrays
+string[] names; // This can reference any size array of strings .
+
+names = new string[4]; // Store items at these index positions 
+
+// Store items at these index positions 
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebecca";
+names[3] = "Tom";
+
+string[] names2 = { "Kate", "Jack", "rebecca", "Tom" };
+
+// Loop through the names 
+for (int i = 0; i < names.Length; i++)
+{
+    // Output the item at index position i
+    WriteLine($"{names2[i]} is at position {i}.");
+}
+#endregion
+
+#region Working with multi dimensional arrays 
+WriteLine();
+
+string[,] grid1 = // Two dimensional array.
+{
+    { "Alpha", "Beta", "Gamma", "Delta" },
+    { "Anne", "Ben", "Charlie", "Doug" },
+    { "Aardvark", "Bear", "Cat", "Dog" }
+};
+
+WriteLine($"1st dimenion, lower bound: {grid1.GetLowerBound(0)}");
+WriteLine($"1st dimension, upper bound: {grid1.GetUpperBound(0)}");
+WriteLine($"2nd dimension, lower bound: {grid1.GetLowerBound(1)}");
+WriteLine($"2nd dimension, upper bound: {grid1.GetUpperBound(1)}");
+
+for (int row = 0; row <= grid1.GetUpperBound(0); row++)
+{
+    for (int col = 0; col <= grid1.GetUpperBound(1); col++)
+    {
+        WriteLine($"Row {row}, Column {col}: {grid1[row, col]}");
+    }
+}
+#endregion
+
+#region Working with jagged arrays 
+WriteLine();
+
+string[][] jagged = // An array of string arrays 
+{
+    new[] { "Alpha", "Beta", "Gamma" },
+    new[] { "Anne", "Ben", "Charlie", "Doug" },
+    new[] { "Aardvark", "Guilty" }
+};
+#endregion
---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,47 @@
+﻿#region Looping with the while statement 
+int x = 0;
+while (x < 10)
+{
+    WriteLine(x);
+    x++;
+}
+#endregion
+
+#region Looping with the do statement
+WriteLine();
+string? actualPassword = "Pa$$w0rd";
+string? password;
+
+do
+{
+    Write("Enter your password: ");
+    password = ReadLine();
+}
+while (password != actualPassword);
+WriteLine("Correct!");
+#endregion
+
+#region Looping with the for statement 
+WriteLine();
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
+
+WriteLine();
+
+for (int y = 0; y <= 10; y += 3)
+{
+    WriteLine(y);
+}
+#endregion
+
+#region Looping with the foreach statement 
+WriteLine();
+
+string[] names = { "Adam", "Barry", "Charlie" };
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
+#endregion
