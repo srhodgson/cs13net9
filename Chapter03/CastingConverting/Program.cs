a9957ffbb550bc218e5bc665c851750c792b04ce
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-11-17 22:25:43 +0000

    casting

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -4,4 +4,13 @@ WriteLine($"a is {a}, b is {b}");
 
 double c = 9.8;
 int d = (int)c; // Compile gives an error if you do not explicitly cast.
-WriteLine($"c is {c}, d is {d}."); // d loses the .8 part
+WriteLine($"c is {c}, d is {d}"); // d loses the .8 part
+
+WriteLine();
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
+e = long.MaxValue;
+f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
