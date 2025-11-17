54f7cf34bf21cd01ce97696e25cef5f6476dc584
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-11-17 22:15:22 +0000

    new project

---
+++ Chapter03/CastingConverting/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 10;
+double b = a; // An int can be safely cast into a double 
+WriteLine($"a is {a}, b is {b}");
+
+double c = 9.8;
+int d = (int)c; // Compile gives an error if you do not explicitly cast.
+WriteLine($"c is {c}, d is {d}."); // d loses the .8 part
