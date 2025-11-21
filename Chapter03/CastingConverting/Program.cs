7419e965298433dd3dce1c168e6ab3577d48ba23
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-11-21 22:56:11 +0000

    How negative numbers are represented in binary
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -8,9 +8,22 @@ WriteLine($"c is {c}, d is {d}"); // d loses the .8 part
 
 WriteLine();
 
-long e = 10;
+long e = 5_000_000_000;
 int f = (int)e;
 WriteLine($"e is {e:N0}, f is {f:N0}");
 e = long.MaxValue;
 f = (int)e;
 WriteLine($"e is {e:N0}, f is {f:N0}");
+
+WriteLine();
+
+#region How negative numbers are represented in binary
+WriteLine("{0,12} {1,34}", "Decimal", "Binary");
+WriteLine("{0,12} {0,34:B32}", int.MaxValue);
+
+for (int i = 8; i >= -8; i--)
+{
+    WriteLine("{0,12} {0,34:B32}", i);
+}
+WriteLine("{0,12} {0,34:B32}", int.MinValue);
+#endregion
