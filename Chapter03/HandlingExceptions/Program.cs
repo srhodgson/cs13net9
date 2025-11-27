5dd317d345b8dc93f285c2110576057ea0c9ef9e
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-11-27 23:18:54 +0000

    try catch
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -6,8 +6,16 @@ try
     int age = int.Parse(input!);
     WriteLine($"You are {age} years old.");
 }
-catch
+catch (OverflowException)
 {
-
+    WriteLine("Your age is a valid number format but it is either too big or small");
+}
+catch (FormatException)
+{
+    WriteLine("The age you are entered is not a valid number format.");
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says {ex.Message}");
 }
 WriteLine("After parsing");
