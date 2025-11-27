43e1e709b2ceca2c632939630aeeff0850c4f521
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-11-27 23:36:54 +0000

    checked and unchecked code examples
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -19,3 +19,36 @@ catch (Exception ex)
     WriteLine($"{ex.GetType()} says {ex.Message}");
 }
 WriteLine("After parsing");
+
+WriteLine();
+
+try
+{
+    checked
+    {
+        int x = int.MaxValue - 1;
+        WriteLine($"Initial value: {x}");
+        x++;
+        WriteLine($"After incrementing {x}");
+        x++;
+        WriteLine($"After incrementing {x}");
+        x++;
+        WriteLine($"After incrementing {x}");
+    }
+}
+catch (OverflowException)
+{
+    WriteLine("The code overflowed but I caught the exception");
+}
+
+WriteLine();
+
+unchecked
+{
+    int y = int.MaxValue + 1;
+    WriteLine($"Initial value: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+}
