d9a4ef8734d2faa6f7cab22777b109df6b7fbb59
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-11-27 23:08:28 +0000

    Add new project for handling exceptions
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ Chapter03/HandlingExceptions/Program.cs
@@ -0,0 +1,13 @@
+﻿WriteLine("Before parsing");
+Write("What is your age? ");
+string? input = ReadLine();
+try
+{
+    int age = int.Parse(input!);
+    WriteLine($"You are {age} years old.");
+}
+catch
+{
+
+}
+WriteLine("After parsing");
