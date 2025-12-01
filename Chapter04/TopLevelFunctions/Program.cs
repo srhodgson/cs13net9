9f37ca813160d121e06050eb50f29f55e75935cd
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-12-01 22:53:50 +0000

    Add a partial class

--- Chapter04/TopLevelFunctions/Program.cs
+++ Chapter04/TopLevelFunctions/Program.cs
@@ -3,9 +3,3 @@
 WriteLine("* Top-Level functions example");
 
 WhatsMyNamespace(); // Call the function;
-
-void WhatsMyNamespace() // Define a local function
-{
-    WriteLine("Namespace of Program class: {0}",
-        arg0: typeof(Program).Namespace ?? "null");
-}
