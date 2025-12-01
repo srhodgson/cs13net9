244fe8cd60b701f8509e645f4fc507e20a8ab298
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-12-01 22:42:18 +0000

    Add new project for top level statements

---
+++ Chapter04/TopLevelFunctions/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+WriteLine("* Top-Level functions example");
+
+WhatsMyNamespace(); // Call the function;
+
+void WhatsMyNamespace() // Define a local function
+{
+    WriteLine("Namespace of Program class: {0}",
+        arg0: typeof(Program).Namespace ?? "null");
+}
