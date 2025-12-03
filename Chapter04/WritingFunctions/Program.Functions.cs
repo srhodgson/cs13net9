23dcfdc5e44c95f31f9c7d75e1363f1ed48e21c2
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-12-03 22:45:08 +0000

    Converting numbers from cardinal to ordinal

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -29,8 +29,7 @@ partial class Program
         return amount * rate;
     }
 
-    static void ConfigureConsole(string culture = "en-US",
-        bool useComputerCulture = false)
+    static void ConfigureConsole(string culture = "en-US", bool useComputerCulture = false)
     {
         // To enable Unicode characters like the Euro symbol in the console 
         OutputEncoding = System.Text.Encoding.UTF8;
@@ -39,5 +38,27 @@ partial class Program
             CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
         }
         WriteLine($"CurrentCulture: {CultureInfo.CurrentCulture.DisplayName}");
+    }
+
+    static string CardinalToOrdinal(uint number)
+    {
+        uint lastTwoDigits = number % 100;
+        switch (lastTwoDigits)
+        {
+            case 11: // Special cases for 11th to 13th.
+            case 12:
+            case 13:
+                return $"{number:N0}th";
+            default:
+                uint lastDigit = number % 10;
+                string suffix = lastDigit switch
+                {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th"
+                };
+                return $"{number:N0}{suffix}";
+        }
     }
 }
--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,3 +1,11 @@
-﻿using System.Globalization; // To use CultureInfo
+﻿using System.Diagnostics;
+using System.Globalization; // To use CultureInfo
 
-TimesTable(7, 20);
+//TimesTable(7, 20);
+
+ConfigureConsole(useComputerCulture: true);
+
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay:C} in tax");
+// Alternatively, call the function in the interpolated string.
+// WriteLine($"You must pay {CalculateTax(amount: 149, twoLetterRegionCode: "FR"):C} in tax.");
