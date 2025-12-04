abc77b90de5dd8282c69f4de17e54e0e441ac3ef
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-12-04 23:57:56 +0000

    Add function to calculate factorial of a number
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -61,4 +61,44 @@ partial class Program
                 return $"{number:N0}{suffix}";
         }
     }
+
+    static void RunCardinalToOrdinal()
+    {
+        for (uint number = 1; number <= 150; number++)
+        {
+            Write($"{CardinalToOrdinal(number)} ");
+        }
+        WriteLine();
+    }
+
+    #region Calculating factorials with recursion
+    static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentOutOfRangeException(message:
+            $"The factorial function is defined for non-negative integers only. Input: {number}", paramName: nameof(number));
+        }
+        else if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            checked
+            {
+                return number * Factorial(number - 1);
+            }
+            
+        }
+    }
+
+    static void RunFactorial()
+    {
+        for (int i = 1; i <= 15; i++)
+        {
+            WriteLine($"{i}! = {Factorial(i):N0!}");
+        }
+    }
+    #endregion
 }
--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -3,9 +3,13 @@ using System.Globalization; // To use CultureInfo
 
 //TimesTable(7, 20);
 
-ConfigureConsole(useComputerCulture: true);
+//ConfigureConsole(useComputerCulture: true);
 
-decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
-WriteLine($"You must pay {taxToPay:C} in tax");
+//decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+//WriteLine($"You must pay {taxToPay:C} in tax");
 // Alternatively, call the function in the interpolated string.
 // WriteLine($"You must pay {CalculateTax(amount: 149, twoLetterRegionCode: "FR"):C} in tax.");
+
+//RunCardinalToOrdinal();
+
+RunFactorial();
