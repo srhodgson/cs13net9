e5f1d2e2de3ab1b1aacea460d87710834892bcea
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-12-16 22:29:54 +0000

    Debugging
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press Enter to end the app.");
+ReadLine();
+
+double Add(double a, double b)
+{
+    return a * b; // Deliberate bug 
+}
+
