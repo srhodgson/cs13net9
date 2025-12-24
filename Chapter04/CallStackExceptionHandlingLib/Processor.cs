d1d54d8cb9d962fcd9c9adde93cfd9cf0b13a29b
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-12-24 22:34:39 +0000

    Finish unit tests start exception handling

---
+++ Chapter04/CallStackExceptionHandlingLib/Processor.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace CallStackExceptionHandlingLib;
+
+public class Processor
+{
+    public static void Gamma() // Public so it can be called for outside.
+    {
+        WriteLine("In Gamma");
+        Delta();
+    }
+
+    private static void Delta() // private so it can only be called internally
+    {
+        WriteLine("In Delta");
+        File.OpenText("bad file path!");
+    }
+}
