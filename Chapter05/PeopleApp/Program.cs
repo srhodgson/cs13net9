3363a48e3d3d0348ba7589910fbf600e4ea39042
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-01-31 21:47:20 +0000

    Pattern matching flight passengers
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -253,3 +253,29 @@ WriteLine($"Sam's first child is {sam[0].Name}.");
 WriteLine($"Sam's second child is {sam[1].Name}.");
 // Get using the string indexer.
 WriteLine($"Sam's child name Ella is {sam["Ella"].Age} years old.");
+
+WriteLine();
+
+// An array containing a mix of passenger types 
+Passenger[] passengers =
+{
+    new FirstClassPassenger { AirMiles = 1_419, Name = "Suman" },
+    new FirstClassPassenger { AirMiles = 16_562, Name = "Lucy" },
+    new BusinessClassPassenger { Name = "Janice" },
+    new CoachClassPassenger { CarryOnKG = 25.7, Name = "Dave" },
+    new CoachClassPassenger { CarryOnKG = 0, Name = "Amit" },
+};
+foreach (Passenger passenger in passengers)
+{
+    decimal flightCost = passenger switch
+    {
+        FirstClassPassenger p when p.AirMiles > 35_000 => 1_500M,
+        FirstClassPassenger p when p.AirMiles > 15_000 => 1_750M,
+        FirstClassPassenger _ => 2_000M,
+        BusinessClassPassenger _ => 1_000M,
+        CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+        CoachClassPassenger _ => 650M,
+        _ => 800M
+    };
+    WriteLine($"Flight costs {flightCost:C} for {passenger}");
+}
