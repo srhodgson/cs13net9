d0b58069b881832e2ec802196996f3b032274a4c
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-01-31 22:30:03 +0000

    Definig record types
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter05/PacktLibraryModern/Records.cs
+++ Chapter05/PacktLibraryModern/Records.cs
@@ -5,3 +5,10 @@ public class ImmutablePerson
     public string? FirstName { get; init; }
     public string? LastName { get; init; }
 }
+
+public record ImmutableVehicle
+{
+    public int Wheels { get; init; }
+    public string? Color { get; init; }
+    public string? Brand { get; init; }
+}
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -296,3 +296,13 @@ ImmutablePerson jeff = new()
     LastName = "Winger"
 };
 // jeff.FirstName = "Geoff";
+
+ImmutableVehicle car = new()
+{
+    Brand = "Mazda MX-5 RF",
+    Color = "Soul Red Crystal Metallic",
+    Wheels = 4
+};
+ImmutableVehicle repaintedCar = car with { Color = "Polymetal Grey Metallic" };
+WriteLine($"Original car color was {car.Color}.");
+WriteLine($"New car color is {repaintedCar.Color}");
