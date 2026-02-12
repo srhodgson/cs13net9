9016d6ccb6d79bccf324910f40a4281b8da18abe
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-02-12 21:58:48 +0000

    Start Chapter 06
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace Packt.Shared;
+
+public class Person
+{
+    #region Properties
+    public string? Name { get; set; }
+    public DateTimeOffset Born { get; set; }
+    public List<Person> Children { get; set; } = new();
+    #endregion
+
+    #region Methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {Born:dddd}.");
+    }
+
+    public void WriteChildrenToConsole()
+    {
+        string term = Children.Count == 1 ? "child" : "children";
+        WriteLine($"{Name} has {Children.Count} {term}.");
+    }
+    #endregion
+}
---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using Packt.Shared;
+
+Person harry = new()
+{
+    Name = "harry",
+    Born = new(year: 2001, month: 3, day: 25, hour: 0, minute: 0, second: 0, offset: TimeSpan.Zero)
+};
+harry.WriteToConsole();
