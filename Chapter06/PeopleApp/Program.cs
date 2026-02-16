543cb206f8ea82a4db7a60bea12eab093117d49a
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-02-16 22:17:48 +0000

    feat: Add demonstration of System.Collections.Hashtable

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -54,5 +54,19 @@ for (int i = 0; i < lamech.Children.Count; i++)
 
 WriteLine();
 
+// Non-generic lookup collection.
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
 
+int key = 2; // Look up the value that has 2 as its key.
+WriteLine(format: "Key {0} has a value: {1}",
+    arg0: key,
+    arg1: lookupObject[key]);
 
+// Look up the value that has harry as its key.
+WriteLine(format: "Key {0} has value: {1}",
+    arg0: harry,
+    arg1: lookupObject[harry]);
