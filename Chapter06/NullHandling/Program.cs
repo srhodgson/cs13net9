7a8855e5d0dcd6c710ab4ce1f2d54ea00cbe7c06
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-02-19 22:55:52 +0000

    Making a value type nullable

---
+++ Chapter06/NullHandling/Program.cs
@@ -0,0 +1,14 @@
+﻿int thisCannotBeNull = 4;
+//thisCannotBeNull = null; // CS0037 compiler error!
+WriteLine(thisCannotBeNull);
+int? thisCouldBeNull = null;
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+thisCouldBeNull = 7;
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+// The actual type of int? is Nullable<int>
+Nullable<int> thisCouldAlsoBeNull = null;
+thisCouldAlsoBeNull = 9;
+WriteLine(thisCouldAlsoBeNull);
