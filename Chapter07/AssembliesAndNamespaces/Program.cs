ef115543aa8ae627d8732678d48ca9e9cee25350
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-03-05 22:31:25 +0000

    Start work on chapter 07

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Xml.Linq; // To use XDocument
 
-Console.WriteLine("Hello, World!");
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World!";
+WriteLine($"{s1} {s2}");
