104b2eb4e403320689cab9cb6f78d0d31928d33f
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-03-20 23:21:44 +0000

    Importing my own package from NuGet, and using it
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,8 +1,24 @@
 ﻿using System.Xml.Linq; // To use XDocument
 using System; // To use String.
+using Packt.Shared;
 
 XDocument doc = new();
 
 string s1 = "Hello";
 String s2 = "World!";
 WriteLine($"{s1} {s2}");
+
+Write("Enter a colour value in hex: ");
+string? hex = ReadLine();
+WriteLine("Is {0} a valid colour value? {1}",
+    arg0: hex, arg1: hex.IsValidHex());
+
+Write("Enter a XML element: ");
+string? xmlTag = ReadLine();
+WriteLine("Is {0} a valid XML element? {1}",
+    arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+
+Write("Enter a password: ");
+string? password = ReadLine();
+WriteLine("Is {0} a valid password? {1}",
+    arg0: password, arg1: password.IsValidPassword());
