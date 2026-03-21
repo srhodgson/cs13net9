c95687e40e7ab18d2d55833d3f614911a6398305
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-03-21 22:13:13 +0000

    Multiplying big numbers
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics; // To use BigInteger.
+
+const int width = 40;
+WriteLine("ulong.MaxValue vs a 30-digit BigInteger");
+WriteLine(new string('-', width));
+ulong big = ulong.MaxValue;
+WriteLine($"{big,width:N0}");
+BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+WriteLine($"{bigger,width:N0}");
+
+WriteLine();
+
+WriteLine("Multiplying big integers");
+int number1 = 2_000_000_000;
+int number2 = 2;
+WriteLine($"number1: {number1:N0}");
+WriteLine($"number2: {number2:N0}");
+WriteLine($"number1 * number2: {number1 * number2:N0}");
+WriteLine($"Math.BigMul(number1, number2): {Math.BigMul(number1, number2):N0}");
+WriteLine($"int.BigMul(number1, number2): {int.BigMul(number1, number2):N0}");
