6f4f061fadcdbf9dd38f7336d459bf7c8da78aae
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-03-21 22:22:50 +0000

    Working with complex numbers
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter08/WorkingWithNumbers/Program.cs
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -18,3 +18,18 @@ WriteLine($"number2: {number2:N0}");
 WriteLine($"number1 * number2: {number1 * number2:N0}");
 WriteLine($"Math.BigMul(number1, number2): {Math.BigMul(number1, number2):N0}");
 WriteLine($"int.BigMul(number1, number2): {int.BigMul(number1, number2):N0}");
+
+WriteLine();
+
+Complex c1 = new(real: 4, imaginary: 2);
+Complex c2 = new(real: 3, imaginary: 7);
+Complex c3 = c1 + c2;
+
+// Output using the default ToString implementation 
+WriteLine($"{c1} added to {c2} is {c3}");
+
+// Output using a custom  format. 
+WriteLine("{0} + {1}i added to {2} + {3}i is {4} + {5}i",
+    c1.Real, c1.Imaginary,
+    c2.Real, c2.Imaginary,
+    c3.Real, c3.Imaginary);
