e3f21feddbce7f86e975e7a3285ce87c4b88a9a1
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-03-23 22:22:07 +0000

    Splitting a string

--- Chapter08/WorkingWithText/Program.cs
+++ Chapter08/WorkingWithText/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string city = "London";
+WriteLine($"{city} is {city.Length} characters long.");
+WriteLine($"First char is {city[0]} and fourth is {city[3]}.");
 
-Console.WriteLine("Hello, World!");
+WriteLine();
+
+string cities = "Paris,Tehran,Chennai,Sydney,New York,Medellin";
+string[] citiesArray = cities.Split(',');
+WriteLine($"There are {citiesArray.Length} cities in the array.");
+foreach (string item in citiesArray)
+{
+    WriteLine($"    {item}");
+}
