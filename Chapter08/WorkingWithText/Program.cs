adad84f01e386c5619821e24781a06da8f6a1a0b
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-03-25 22:50:21 +0000

    Comparing string values
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter08/WorkingWithText/Program.cs
+++ Chapter08/WorkingWithText/Program.cs
@@ -1,4 +1,7 @@
-﻿string city = "London";
+﻿using System.Globalization; // To use CultureInfo
+OutputEncoding = System.Text.Encoding.UTF8; // Enable Euro Symbol
+
+string city = "London";
 WriteLine($"{city} is {city.Length} characters long.");
 WriteLine($"First char is {city[0]} and fourth is {city[3]}.");
 
@@ -29,3 +32,27 @@ string company = "Microsoft";
 WriteLine($"Text: {company}");
 WriteLine($"Starts with M: {0}, contains an N: {1}",
     arg0: company.StartsWith('M'), arg1: company.Contains('N'));
+
+WriteLine();
+
+CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-GB");
+string text1 = "Mark";
+string text2 = "MARK";
+WriteLine($"text1: {text1}, text2: {text2}");
+WriteLine("Compare: {0}.", string.Compare(text1, text2));
+WriteLine("Compare (ignoreCase): {0}.",
+    string.Compare(text1, text2, ignoreCase: true));
+WriteLine("Compare (InvariantCultureIgnoreCase): {0}.",
+    string.Compare(text1, text2, StringComparison.InvariantCultureIgnoreCase));
+// German string comparisons 
+CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
+text1 = "Strasse";
+text2 = "Straße";
+WriteLine($"text1: {text1}, text2: {text2}");
+WriteLine("Compare: {0}.", string.Compare(text1, text2,
+    CultureInfo.CurrentCulture, CompareOptions.IgnoreNonSpace));
+WriteLine("Compare (IgnoreCase, IgnoreNonSPace): {0}.",
+    string.Compare(text1, text2, CultureInfo.CurrentCulture,
+    CompareOptions.IgnoreNonSpace | CompareOptions.IgnoreCase));
+WriteLine("Compare (InvariantCultureIgnoreCase): {0}.",
+    string.Compare(text1, text2, StringComparison.InvariantCultureIgnoreCase));
