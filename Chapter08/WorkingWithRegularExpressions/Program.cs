d837d65c36a51055ff11791b99a2989b4f154231
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-04-06 22:18:06 +0100

    updated:   Chapter08/WorkingWithText/WorkingWithText.csproj
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

--- Chapter08/WorkingWithRegularExpressions/Program.cs
+++ Chapter08/WorkingWithRegularExpressions/Program.cs
@@ -7,3 +7,28 @@ Regex ageChecker = new(@"^\d+$"); // Check that the only thing between the start
 WriteLine(ageChecker.IsMatch(input) ? "Thank you!" :
     $"This is not a valid age: {input}");
 
+WriteLine();
+
+// C# 1 to 10: Use escaped double-quote characters \"string films = "\"Monsters, Inc.\",
+// \"I, Tonya\",\"Lock, Stock and Two Smoking Barrels\"";
+// C# 11 or later: Use """ to start and a raw string literal
+string films = """
+    "Monsters, Inc","I, Tonya","Lock, Stock, and Two Smoking Barrels"
+""";
+WriteLine($"Films to split: {films}");
+string[] filmsDumb = films.Split(',');
+WriteLine("Splitting with string.Split method:");
+foreach (string film in filmsDumb)
+{
+    WriteLine($"    {film}");
+}
+
+Regex csv = new(
+    "(?:^|,)(?=[^\"]|(\")?)\"?((?(1)[^\"]*|[^,\"]*))\"?(?=,|$)");
+MatchCollection filmSmart = csv.Matches(films);
+WriteLine("Splitting with regular expression:");
+foreach(Match film in filmSmart)
+{
+    WriteLine($"    {film.Groups[2].Value}");
+}
+
