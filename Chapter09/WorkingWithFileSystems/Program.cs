35ee2f26262ce35bcde5ec72e12865269b7575d0
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-04-26 23:29:52 +0100

    Start chapter09
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ Chapter09/WorkingWithFileSystems/Program.cs
@@ -0,0 +1,25 @@
+﻿using Spectre.Console; // To use Table 
+
+#region Handling cross-platform environemnts and filesystems
+SectionTitle("Handling cross-platform environments and filesystems");
+// Create a Spectre Console table.
+Table table = new();
+// Add two columns with markup for colors.
+table.AddColumn("[blue]MEMBER[/]");
+table.AddColumn("[blue]VALUE[/]");
+// Add rows 
+table.AddRow("Path.PathSeparator", PathSeparator.ToString());
+table.AddRow("Path.DirectorySeparator", DirectorySeparatorChar.ToString());
+table.AddRow("Directory.GetCUrrentDirectory()", GetCurrentDirectory());
+table.AddRow("Environment.CurrentDirectory", CurrentDirectory);
+table.AddRow("Environment.SystemDirectory", SystemDirectory);
+table.AddRow("Path.GetTempPath()", GetTempPath());
+table.AddRow("");
+table.AddRow("GetFolderPath(SpecialFolder", "");
+table.AddRow("  .System)", GetFolderPath(SpecialFolder.System));
+table.AddRow("  .Application)", GetFolderPath(SpecialFolder.ApplicationData));
+table.AddRow("  .MyDocuments)", GetFolderPath(SpecialFolder.MyDocuments));
+table.AddRow("  .Personal)", GetFolderPath(SpecialFolder.Personal));
+// Render the table to the console.
+AnsiConsole.Write(table);
+#endregion
