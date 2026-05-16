0d8a138d6690648304986b35baf2983c1c0b04fc
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-05-16 21:56:28 +0100

    Writing text to streams

--- Chapter09/WorkingWIthStreams/Program.cs
+++ Chapter09/WorkingWIthStreams/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared; // To use Viper
+
+SectionTitle("Writing to text streams");
+// Define a file to write to.
+string textFile = Combine(CurrentDirectory, "streams.txt");
+// Create a text file and return a helper writer.
+StreamWriter text = File.CreateText(textFile);
+// Enumerate the strings, writing each one to the stream
+// on a separate line.
+foreach (string item in Viper.Callsigns)
+{
+    text.WriteLine(item);
+}
+text.Close(); // Release unmanaged file resources 
+OutputFileInfo(textFile);
---
+++ Chapter09/WorkingWIthStreams/Viper.cs
@@ -0,0 +1,11 @@
+namespace Packt.Shared;
+
+public static class Viper
+{
+    // Define an array of Viper pilot callsigns 
+    public static string[] Callsigns = new[]
+    {
+        "Husker", "Starbuck", "Apollo", "Boomer",
+        "Bulldog", "Athena", "Helo", "Racetrack"
+    };
+}
