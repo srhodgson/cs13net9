05f68acdcb408ed4c7a562b64ff219f8d98e11cf
Stephen <stephen@srhodgson.co.uk>
2026-06-27 19:20:02 +0100

    looking at real numbers and comparing decimal;s and doublesd
    Signed-off-by: Stephen <stephen@srhodgson.co.uk>

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -15,6 +15,8 @@ float realNumber = 2.3f;
 double anotherRealNumber = 2.3; // A double literal value 
 #endregion
 
+Console.WriteLine("----------");
+
 #region Exploring whole numbers
 int decimalNotation = 2_000_000;
 int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
@@ -23,14 +25,54 @@ int hexadecimalNotation = 0x_001E_8480;
 // Check the three variables have the same value.
 Console.WriteLine($"{decimalNotation == binaryNotation}");
 Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
-
+Console.WriteLine();
 // Output the values in decimal
 Console.WriteLine($"{decimalNotation:N0}");
 Console.WriteLine($"{binaryNotation:N0}");
 Console.WriteLine($"{hexadecimalNotation:N0}");
-
+Console.WriteLine();
 // Output the variable values in hexadecimal 
 Console.WriteLine($"{decimalNotation:X}");
 Console.WriteLine($"{binaryNotation:X}");
 Console.WriteLine($"{hexadecimalNotation:X}");
+#endregion
+
+Console.WriteLine("----------");
+
+#region Storing real numbers
+Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}");
+Console.WriteLine($"int uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}");
+Console.WriteLine($"int uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
+#endregion
+
+Console.WriteLine("----------");
+
+#region Comparing double and decimal types
+Console.WriteLine("Using doubles:");
+double a = 0.1;
+double b = 0.2;
+
+if (a + b == 0.3)
+{
+    Console.WriteLine($"{a} + {b} equals {0.3}");
+}
+else
+{
+    Console.WriteLine($"{a} + {b} does NOT equal {0.3}");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Using decimals:");
+decimal c = 0.1M;
+decimal d = 0.2M;
+
+if (c + d == 0.3M)
+{
+    Console.WriteLine($"{c} + {d} equals {0.3M}");
+}
+else
+{
+    Console.WriteLine($"{c} + {d} does NOT equal {0.3M}");
+}
 #endregion
