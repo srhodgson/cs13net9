714b5d463fac94163b634cb062ee69a1670ba167
Stephen <stephen@srhodgson.co.uk>
2026-06-27 13:10:27 +0100

    Working with numbers

---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿#region Storinhg numbers
+// An unsigned integer is a positive whole number or 0
+uint naturalNumber = 23;
+
+// An integer is a negative or positive whole number or 0;
+int integerNumber = -23;
+
+// A float is a single-precision floating-point number.
+// The F or f is suffix makes the value to a float literal.
+// The suffix is required to compile.
+float realNumber = 2.3f;
+
+// A double is a double-precision floating point number.
+// double is the default for a number value with a decimal point.
+double anotherRealNumber = 2.3; // A double literal value 
+#endregion
+
+#region Exploring whole numbers
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+// Check the three variables have the same value.
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+
+// Output the values in decimal
+Console.WriteLine($"{decimalNotation:N0}");
+Console.WriteLine($"{binaryNotation:N0}");
+Console.WriteLine($"{hexadecimalNotation:N0}");
+
+// Output the variable values in hexadecimal 
+Console.WriteLine($"{decimalNotation:X}");
+Console.WriteLine($"{binaryNotation:X}");
+Console.WriteLine($"{hexadecimalNotation:X}");
+#endregion
