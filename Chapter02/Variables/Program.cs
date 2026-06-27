b8dd259686c252527b9871dffbbd015f87973eb5
Stephen <stephen@srhodgson.co.uk>
2026-06-27 19:53:33 +0100

    Storing any type of object
    Signed-off-by: Stephen <stephen@srhodgson.co.uk>

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -6,3 +6,23 @@ Console.WriteLine($"{name} is {height} metres tall");
 int length2 = ((string)name).Length; // Cast name to a string 
 Console.WriteLine($"{name} has {length2} characters.");
 #endregion
+
+Console.WriteLine();
+
+#region Storing dynamic types
+
+dynamic something;
+// Storing an array of int values in a dynamic object.
+// An array of any type has a Length property.
+something = new[] { 3, 5, 7 };
+// Storing an int in a dynamic object.
+// int does not have a Length property.
+// something = 12;
+//Storing a string in a dynamic object.
+// string has a Length property.
+// something = "Ahmed";
+// This compiles but might throw an exception at run-time. 
+Console.WriteLine($"The length of something is {something.Length}");
+// Output the type of the something's variable 
+Console.WriteLine($"something is a {something.GetType()}");
+#endregion
