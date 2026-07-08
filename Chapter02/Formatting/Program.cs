b5f458e8ed368a1633b1f10723150a99b730dfbc
Stephen <stephen@srhodgson.co.uk>
2026-07-08 21:04:34 +0100

    Finishing off the project

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -6,7 +6,7 @@ CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
 #region Displaying output to the user
 int numberOfApples = 12;
 decimal pricePerApple = 0.35M;
-Console.WriteLine(
+WriteLine(
     format: "{0} apples cost {1:C}",
     arg0: numberOfApples,
     arg1: pricePerApple * numberOfApples);
@@ -18,48 +18,58 @@ string formatted = string.Format(
 // WriteToFile(formatted); // Writes the string into a file.    
 
 // Three parameter values can use named arguments 
-Console.WriteLine("{0} {1} lived in {2}.",
+WriteLine("{0} {1} lived in {2}.",
     arg0: "Roger", arg1: "Cevung", arg2: "Stockholm");
 // Four or more parameters values cannot use named arguments 
-Console.WriteLine("{0} {1} lived in {2} and worked in the {3} team at {4}.",
+WriteLine("{0} {1} lived in {2} and worked in the {3} team at {4}.",
     "Roger", "Cevung", "Stockholm", "Education", "Optimizely");
 #endregion
-Console.WriteLine();
+WriteLine();
 
 #region Formatting using interpolated strings
 // The following statement must be all on one line when using C# 10 or earlier. If using C# 11 or later, we can include 
 // a line break in the middle of an expression but not in the string text.
-Console.WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
+WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
 #endregion
-Console.WriteLine();
+WriteLine();
 
 #region Understanding format strings 
 string applesText = "Apples";
 int applesCount = 1234;
 string bananasText = "Bananas";
 int bananasCount = 56789;
-Console.WriteLine();
-Console.WriteLine(format: "{0,-10} {1,6}",
+WriteLine();
+WriteLine(format: "{0,-10} {1,6}",
 arg0: "Name", arg1: "Count");
 
-Console.WriteLine(format: "{0,-10} {1,6:N0}",
+WriteLine(format: "{0,-10} {1,6:N0}",
 arg0: applesText, arg1: applesCount);
 
-Console.WriteLine(format: "{0,-10} {1,6:N0}",
+WriteLine(format: "{0,-10} {1,6:N0}",
 arg0: bananasText, arg1: bananasCount);
 #endregion
-Console.WriteLine();
+WriteLine();
 
 #region Custom number formatting 
 decimal value = 0.325M;
-Console.WriteLine("Currency: {0:C}, Percentage: {0:0.0%}", value);
+WriteLine("Currency: {0:C}, Percentage: {0:0.0%}", value);
 #endregion
-Console.WriteLine();
+WriteLine();
 
 #region Getting input from the user 
-Console.Write("Type your first name and press ENTER: ");
-string? firstName = Console.ReadLine();
-Console.Write("Type your age and press ENTER: ");
-string age = Console.ReadLine()!;
-Console.WriteLine($"Hello {firstName}, you look good for {age}.");
+Write("Type your first name and press ENTER: ");
+string? firstName = ReadLine();
+Write("Type your age and press ENTER: ");
+string age = ReadLine()!;
+WriteLine($"Hello {firstName}, you look good for {age}.");
+#endregion  
+
+WriteLine();
+
+#region Getting key input from the user 
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+arg0: key.Key, arg1: key.KeyChar, arg2: key.Modifiers);
 #endregion
