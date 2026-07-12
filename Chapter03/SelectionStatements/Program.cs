061a689de4b766ae9fd167731855413033208c17
Stephen <stephen@srhodgson.co.uk>
2026-07-12 22:35:38 +0100

    Start the next project

---
+++ Chapter03/SelectionStatements/Program.cs
@@ -0,0 +1,9 @@
+﻿string password = "ninja";
+if (password.Length < 8)
+{
+    WriteLine("Your password is too short. Use at least 8 chars.");
+}
+else
+{
+    WriteLine("Your password is strong.");
+}
