69c2c7b4fdaaca575ec01389475abf49b1618df1
Stephen <stephen@srhodgson.co.uk>
2026-07-19 23:53:16 +0100

    Pattern matching with the switch statement

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -1,4 +1,6 @@
-﻿string password = "ninja";
+﻿using SelectionStatements;
+
+string password = "ninja";
 if (password.Length < 8)
 {
     WriteLine("Your password is too short. Use at least 8 chars.");
@@ -51,4 +53,64 @@ switch (number)
 } // End of switch statement 
 A_label:
 WriteLine($"After A_label");
+#endregion
+
+WriteLine();
+
+#region Pattern matching with the switch statement
+
+var animals = new Animal?[]
+{
+    new Cat
+    {
+        Name = "Karen",
+        Born = new(year: 2022, month: 8, day: 23),
+        Legs = 4,
+        IsDomestic = true
+    },
+    null,
+    new Cat
+    {
+        Name = "Mufasa",
+        Born = new(year: 1994, month: 6, day: 12)
+    },
+    new Spider
+    {
+        Name = "Sid Vicious",
+        Born = DateTime.Today,
+        IsVenomous = true
+    },
+    new Spider
+    {
+        Name = "Captain Furry",
+        Born = DateTime.Today
+    }
+};
+
+foreach (Animal? animal in animals)
+{
+    string message;
+    switch (animal)
+    {
+        case Cat fourLeggedCat when fourLeggedCat.Legs == 4:
+            message = $"The cat named {fourLeggedCat.Name} has four legs.";
+            break;
+        case Cat wildCat when wildCat.IsDomestic == false:
+            message = $"The non-domestic cat is name {wildCat.Name}.";
+            break;
+        case Cat cat:
+            message = $"The cat is named {cat.Name}.";
+            break;
+        default: // default is always evaluated last.
+            message = $"{animal.Name} is a {animal.GetType().Name}.";
+            break;
+        case Spider spider when spider.IsVenomous:
+            message = $"The {spider.Name} is venomous. Run!";
+            break;
+        case null:
+            message = "The animal is null.";
+            break;
+    }
+    WriteLine($"switch statement: {message}");
+}
 #endregion
