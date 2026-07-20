6ef148fe1cca1357390bb91949b20d9e9dac855c
Stephen <stephen@srhodgson.co.uk>
2026-07-20 22:51:45 +0100

    looping with the foreach statement

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,38 @@
+﻿int x = 0;
+while (x < 10)
+{
+    WriteLine(x);
+    x++;
+}
+
+string? actualPassword = "Pa$$w0rd";
+string? password;
+
+//do
+//{
+//    Write("Enter your password: ");
+//    password = ReadLine();
+//} while (password != actualPassword);
+//WriteLine("Correct!");
+WriteLine();
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
+
+WriteLine();
+
+for (int y = 0; y <= 10; y += 3)
+{
+    WriteLine(y);
+}
+
+WriteLine();
+
+string[] names = { "Adam", "Barry", "Charlie" };
+
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
