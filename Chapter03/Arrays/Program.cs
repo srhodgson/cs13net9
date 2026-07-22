5f39dee173a514387888387a42d339e668bced7b
Stephen <stephen@srhodgson.co.uk>
2026-07-22 22:36:55 +0100

    Working with multi-dimensional arrays

--- Chapter03/Arrays/Program.cs
+++ Chapter03/Arrays/Program.cs
@@ -15,3 +15,28 @@ for (int i = 0; i < names.Length; i++)
     // output the item at the index position i.
     WriteLine($"{names[i]} is at position {i}.");
 }
+
+WriteLine();
+
+#region Working with multi-dimensional arrays
+
+string[,] grid1 = // two dimensional array
+{
+    { "Alpha", "Beta", "Gamma", "Delta" },
+    { "Anne", "Ben", "Charlie", "Doug" },
+    { "Aardvark", "Bear", "Cat", "Dog" }
+};
+
+WriteLine($"1st dimension, lower bound: {grid1.GetLowerBound(0)}");
+WriteLine($"1st dimension, upper bound: {grid1.GetUpperBound(0)}");
+WriteLine($"2nd dimension, lower bound: {grid1.GetLowerBound(1)}");
+WriteLine($"2nd dimension, upper bound: {grid1.GetUpperBound(1)}");
+
+for (int row = 0; row <= grid1.GetUpperBound(0); row++)
+{
+    for (int col = 0; col <= grid1.GetUpperBound(1); col++)
+    {
+        WriteLine($"Row {row}, Column {col}: {grid1[row, col]}");
+    }
+}
+#endregion
