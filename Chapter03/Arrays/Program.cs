14c52fba420667cbdc2bae42b33518a1c840c6de
Stephen <stephen@srhodgson.co.uk>
2026-07-22 22:48:39 +0100

    Working with jagged arrays

--- Chapter03/Arrays/Program.cs
+++ Chapter03/Arrays/Program.cs
@@ -32,11 +32,47 @@ WriteLine($"1st dimension, upper bound: {grid1.GetUpperBound(0)}");
 WriteLine($"2nd dimension, lower bound: {grid1.GetLowerBound(1)}");
 WriteLine($"2nd dimension, upper bound: {grid1.GetUpperBound(1)}");
 
+WriteLine();
+
 for (int row = 0; row <= grid1.GetUpperBound(0); row++)
 {
     for (int col = 0; col <= grid1.GetUpperBound(1); col++)
     {
         WriteLine($"Row {row}, Column {col}: {grid1[row, col]}");
+    }
+}
+#endregion
+
+WriteLine();
+
+#region Working jagged arrays
+
+string[][] jagged = // An array of string arrays 
+{
+    new[] { "Alpha", "Beta", "Gamma" },
+    new[] { "Anne", "Ben", "Charlie", "Doug" },
+    new[] { "Aardvark", "Bear" }
+};
+
+WriteLine("Upper bound of the array of arrays is: {0}", 
+    jagged.GetUpperBound(0));
+
+WriteLine();
+
+for (int array = 0; array <= jagged.GetUpperBound(0); array++)
+{
+    WriteLine("Upper bound of array {0} is: {1}",
+        arg0: array,
+        arg1: jagged[array].GetUpperBound(0));
+}
+
+WriteLine();
+
+for (int row = 0; row <= jagged.GetUpperBound(0); row++)
+{
+    for (int col = 0; col <= jagged[row].GetUpperBound(0); col++)
+    {
+        WriteLine($"Row {row}, Column {col}: {jagged[row][col]}");
     }
 }
 #endregion
