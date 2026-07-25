9fa678ca8605a39a4c84f3b0434fd90135792e73
Stephen <stephen@srhodgson.co.uk>
2026-07-25 20:57:38 +0100

    finish off casting and converting

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -6,4 +6,12 @@ WriteLine($"a is {a}, b is {b}");
 double c = 9.8;
 int d = (int)c; // Compiler gives an error if you do not explicitly cast.
 WriteLine($"c is {c}, d is {d}"); // d loses the .8 part.
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
+
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
 #endregion
