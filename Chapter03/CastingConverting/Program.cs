28157ca3a34c66553d8a9c5640194b17657f51a1
Stephen <stephen@srhodgson.co.uk>
2026-07-25 21:22:53 +0100

    Rounding number and the default rounding rules

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -46,3 +46,30 @@ int h = ToInt32(g); // A method of System.Convert
 WriteLine($"g is {g}, h is {h}");
 #endregion
 
+WriteLine();
+
+#region Rounding number and the default rounding rules
+
+double[,] doubles =
+{
+    { 9.49, 9.5, 9.52 },
+    { 10.49, 10.5, 10.51 },
+    { 11.49, 11.5, 11.51 },
+    { 12.49, 12.5, 12.51 },
+    { -12.49, -12.5, -12.51 },
+    { -11.49, -11.5, -11.51 },
+    { -10.49, -10.5, -10.51 },
+    { -9.49, -9.5, -9.51 },
+};
+WriteLine($"| double | ToInt32 | double | ToInt32 | double | ToInt32 |");
+for (int x = 0; x < 8; x++)
+{
+    for (int y = 0; y < 3; y++)
+    {
+        Write($"| {doubles[x, y],6} | {ToInt32(doubles[x, y]),7}");
+    }
+    WriteLine("|");
+}
+
+WriteLine();
+#endregion
