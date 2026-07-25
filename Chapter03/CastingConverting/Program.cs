2bc63cb0749f48f37d88783e5c47d55935557523
Stephen <stephen@srhodgson.co.uk>
2026-07-25 21:44:03 +0100

    Converting from a binary object to a string

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -98,3 +98,22 @@ WriteLine(now.ToString());
 object me = new();
 WriteLine(me.ToString());
 #endregion
+
+WriteLine();
+
+#region Converting from a binary object to a string 
+// Allocate an array of 128 bytes.
+byte[] binaryObject = new byte[128];
+// Populate the array with random bytes 
+Random.Shared.NextBytes(binaryObject);
+WriteLine("Binary Object as bytes:");
+for (int index = 0; index < binaryObject.Length; index++)
+{
+    Write($"{binaryObject[index]:X2}  ");
+}
+
+WriteLine();
+// Convert the array to Base64 string and output as text.
+string encoded = ToBase64String(binaryObject);
+WriteLine($"Binary Object as Base64: {encoded}");
+#endregion
