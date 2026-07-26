2049e554f74e1eb3e43e8775877c88c9f501eeb1
Stephen <stephen@srhodgson.co.uk>
2026-07-26 21:27:45 +0100

    More error handling

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -1,13 +1,33 @@
-﻿WriteLine("Before parsing");
-Write("What is your age? ");
-string? input = ReadLine();
-try
+﻿//WriteLine("Before parsing");
+//Write("What is your age? ");
+//string? input = ReadLine();
+//try
+//{
+//    int age = int.Parse(input!);
+//    WriteLine($"You are {age} years old.");
+//}
+//catch (OverflowException)
+//{
+//    WriteLine("Your age is a valid number format but it is either too big or small.");
+//}
+//catch (FormatException)
+//{
+//    WriteLine("The age you entered is not a valid number format.");
+//}
+//catch (Exception ex)
+//{   
+//    WriteLine($"{ex.GetType()} says {ex.Message}");
+//}
+//WriteLine("After parsing");
+
+checked
 {
-    int age = int.Parse(input!);
-    WriteLine($"You are {age} years old.");
+    int x = int.MaxValue - 1;
+    WriteLine($"Initial value: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}");
+    x++;
+    WriteLine($"After after incrementing: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}");
 }
-catch
-{
-    
-}
-WriteLine("After parsing");
