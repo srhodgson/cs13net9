3484717ca53869914c6638c1dbf0f06219a369cc
Stephen <stephen@srhodgson.co.uk>
2026-08-07 23:16:43 +0100

    start chapter exercises

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -19,15 +19,31 @@
 //    WriteLine($"{ex.GetType()} says {ex.Message}");
 //}
 //WriteLine("After parsing");
-
-checked
+try
 {
-    int x = int.MaxValue - 1;
-    WriteLine($"Initial value: {x}");
-    x++;
-    WriteLine($"After incrementing: {x}");
-    x++;
-    WriteLine($"After after incrementing: {x}");
-    x++;
-    WriteLine($"After incrementing: {x}");
+    checked
+    {
+        int x = int.MaxValue - 1;
+        WriteLine($"Initial value: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After after incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+    }
+}
+catch (OverflowException)
+{
+    WriteLine("The code overflowed but I caught the exception.");
+}
+
+unchecked
+{
+    int y = int.MaxValue + 1;
+    WriteLine($"Initial value: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
 }
