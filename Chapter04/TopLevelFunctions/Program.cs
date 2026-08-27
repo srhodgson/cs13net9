7fd9343e141fb43c569e1752b2fad8470b3d0b14
Stephen <stephen@srhodgson.co.uk>
2026-08-27 22:28:49 +0100

    Start first program

---
+++ Chapter04/TopLevelFunctions/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+WriteLine("* Top-Level functions examples");
+
+WhatsMyNameSpace(); // Call the function.
+
+void WhatsMyNameSpace()
+{
+    WriteLine("Namespace of Program class: {0}",
+        arg0: typeof(Program).Namespace ?? "null");
+}
