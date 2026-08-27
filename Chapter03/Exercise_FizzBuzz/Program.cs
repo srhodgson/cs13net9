404518105cd8716d5841a79791a8f39743cd377a
Stephen <stephen@srhodgson.co.uk>
2026-08-27 22:15:05 +0100

    Finish FizzBuzz and start chapter04

--- Chapter03/Exercise_FizzBuzz/Program.cs
+++ Chapter03/Exercise_FizzBuzz/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.InteropServices;
 
-Console.WriteLine("Hello, World!");
+int number = 1;
+
+while (number <= 100)
+{
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        Write("FizzBuzz");
+    }
+    else if (number % 5 == 0)
+    {
+        Write("Buzz");
+    }
+    else if (number % 3 == 0)
+    {
+        Write("Fizz");
+    }
+    else
+    {
+        Write(number);
+    }
+    
+    if (number < 100)
+        Write(", ");
+    number++;
+}
