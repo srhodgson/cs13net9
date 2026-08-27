82440e0d2f64170053c10b783b79d4c41b1f41d8
Stephen <stephen@srhodgson.co.uk>
2026-08-27 23:16:26 +0100

    Writing functions

---
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -0,0 +1,14 @@
+partial class Program
+{
+    static void TimesTable(byte number, byte size = 12)
+    {
+        WriteLine($"This is the {number} time table with {size} rows:");
+        WriteLine();
+        for (int row = 1; row <= size; row++)
+        {
+            WriteLine($"{row} x {number} = {row * number}");
+        }
+
+        WriteLine();
+    }
+}
