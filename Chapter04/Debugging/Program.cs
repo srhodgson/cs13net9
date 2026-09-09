911298a0d410552ab028558d440ebf72cc0d9af5
Stephen <stephen@srhodgson.co.uk>
2026-09-09 22:52:08 +0100

    Start debugging project

--- Chapter04/Debugging/Program.cs
+++ Chapter04/Debugging/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press Enter to end the app.");
+ReadLine();
+
+double Add(double a, double b)
+{
+    return a * b; // Deliberate bug!
+}
