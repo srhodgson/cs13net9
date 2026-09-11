560dadb2ae6798e408309b674c56c6d9d8952bd5
Stephen <stephen@srhodgson.co.uk>
2026-09-11 22:05:35 +0100

    update to fix bug and to allow user to enter amount for a

--- Chapter04/Debugging/Program.cs
+++ Chapter04/Debugging/Program.cs
@@ -1,4 +1,6 @@
-double a = 4.5;
+Write("Enter a number: ");
+string number = ReadLine();
+double a = double.Parse(number);
 double b = 2.5;
 double answer = Add(a, b);
 WriteLine($"{a} + {b} = {answer}");
@@ -7,5 +9,5 @@ ReadLine();
 
 double Add(double a, double b)
 {
-    return a * b; // Deliberate bug!
+    return a + b; // Deliberate bug!
 }
