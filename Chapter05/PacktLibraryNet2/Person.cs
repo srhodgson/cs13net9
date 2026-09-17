87c1b7ddefcd6d5e56ff17d02895de5eb5f3f46b
Stephen <stephen@srhodgson.co.uk>
2026-09-17 09:06:27 +0100

    Enums

--- Chapter05/PacktLibraryNet2/Person.cs
+++ Chapter05/PacktLibraryNet2/Person.cs
@@ -3,5 +3,10 @@ namespace Packt.Shared;
 
 public class Person : object
 {
-    
+    #region Fields: Data or state for this person.
+    public string? Name; // ? means it can be null.
+    public DateTimeOffset Born;
+    public WondersOfTheAncientWorld FavouriteAncientWonder;
+    public WondersOfTheAncientWorld BucketList;
+    #endregion
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -9,3 +9,33 @@ ConfigureConsole(); // Sets current culture to US English.
 Person bob = new(); // C# 9 or later.
 WriteLine(bob); // Implicit call ToString().
 // WriteLine(bob.ToString()); // Does the same thing 
+
+bob.Name = "Bob Smith";
+bob.Born = new DateTimeOffset(
+    year: 1965, month: 12, day: 22,
+    hour: 16, minute: 28, second: 0,
+    offset: TimeSpan.FromHours(-5)); // US Eastern Standard Time
+bob.FavouriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+// bob.BucketList = (WondersOfTheAncientWorld)18;
+
+WriteLine(format: "{0} was born on {1:D}.", // Long date.
+    arg0: bob.Name, arg1: bob.Born);
+WriteLine(format: "{0}'s favourite wonder is {1}. Its integer is {2}.",
+    arg0: bob.Name,
+    arg1: bob.FavouriteAncientWonder,
+    arg2: (int)bob.FavouriteAncientWonder);
+WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}.");
+
+WriteLine();
+
+Person alice = new()
+{
+    Name = "Alice Jones",
+    Born = new(1998, 3, 7, 16, 28, 0,
+        // This is an optional offset from UTC time zone
+        TimeSpan.Zero)
+};
+
+WriteLine(format: "{0} was born on {1:d}.",
+    arg0: alice.Name, arg1: alice.Born);
